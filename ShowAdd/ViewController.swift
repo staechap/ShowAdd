9bd96ed16402737de833354b9ede85e49e13e741
staechap <mon.oracle@gmail.com>
2017-09-09 11:56:36 +0700

    Initial Commit

---
+++ ShowAdd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowAdd
+//
+//  Created by Sunisa Kirtsopa on 9/9/2560 BE.
+//  Copyright © 2560 oramon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
