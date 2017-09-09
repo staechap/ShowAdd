ea44b3310d46b601ef11d71d69203bb2445ab9f2
staechap <mon.oracle@gmail.com>
2017-09-09 13:34:48 +0700

    finished Layout

--- ShowAdd/ViewController.swift
+++ ShowAdd/ViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
