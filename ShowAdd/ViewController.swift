c242c4ff199efd04063798f651f6e292fa636310
staechap <mon.oracle@gmail.com>
2017-09-09 14:17:56 +0700

    Finished

--- ShowAdd/ViewController.swift
+++ ShowAdd/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //Implisit
+    
+    var xString = ""
+    var yString = ""
+    
+    @IBOutlet weak var inputText1Outlet: UITextField!
+    
+    
+    @IBOutlet weak var inputText2Outlet: UITextField!
     
     
     
+    @IBOutlet weak var answerOutlet: UILabel!
     
     
-    
-    
-    
-    
-    
+    @IBAction func calAction(_ sender: Any) {
+        
+        // Get value from textfield
+        
+        xString = inputText1Outlet.text!
+        yString = inputText2Outlet.text!
+        
+        print("input1 Value==> \(xString)")
+        print("input2 Value==> \(yString)")
+        
+        //Change data Type
+        
+        let xAInt = Int(xString)
+        let yAInt = Int(yString)
+        
+        let answerAInt  =   xAInt!+yAInt!
+        
+        let answerString = String(answerAInt)
+        
+        answerOutlet.text = answerString
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
